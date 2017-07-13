a75d72ea1e7f49248960474ece90eed1032256ed
jbbpatel94 <jbbpatel94@gmail.com>
2017-07-13 19:42:41 +0530

    added css file set awards dynamically

--- client/main.js
+++
@@ -1,7 +0,0 @@
-Template.addPlayer.events({
-	'submit form': function(evt){
-		evt.preventDefault();
-		console.log("Form submitted");
-		console.log(event.type);
-	}
-});
--- client/templates/player_list.js
+++ client/templates/player_list.js
@@ -1,7 +1,34 @@
-var data = [{name: "Jaisa",score :0},
-{name: "Vikash",score :0},
-{name: "Manish",score :0},
-{name: "Satyam",score :0}];
 Template.playersList.helpers({
-	player : data
+	player : function(){
+		return Players.find({},{sort:{score:-1,name :+1}});
+	}
+});
+Template.players.helpers({
+	'selectedClass': function(){
+		var playerId = this._id;
+		var selectedPlayer = Session.get('selectedPlayer');
+		if(playerId == selectedPlayer){
+			return "selected"
+		}
+	}
+});
+Template.body.events({
+	'submit form': function(evt){
+		evt.preventDefault();
+		var playerName = evt.target.playerName.value;
+		evt.target.playerName.value = "";
+		Players.insert({name:playerName,score:0})
+	},
+	'click .player':function(){
+		Session.set('selectedPlayer',this._id);
+		//console.log(this._id);
+	},
+	'click .increment': function(){
+		var selectedPlayer = Session.get('selectedPlayer');
+		Players.update(selectedPlayer, {$inc: {score: 5} });
+	},
+	'click .decrement': function(){
+		var selectedPlayer = Session.get('selectedPlayer');
+		Players.update(selectedPlayer, {$inc: {score: -5} });
+	}
 });
